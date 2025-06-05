71d1133c2017abd5adc07b39d9abffc61dc3958d
Stalin Patel <stalinpatel023@gmail.com>
2025-06-05 12:30:34 +0530

    basic setup done

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+const tailwindConfig = {
+  // In v4 you can often skip content, but you can include it if needed:
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFA500 ",
+      },
+    },
+  },
+  plugins: [
+    /* e.g. require('@tailwindcss/forms') */
+  ],
+};
+export default tailwindConfig;
